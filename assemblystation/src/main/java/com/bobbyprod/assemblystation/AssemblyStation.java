58be3bcda14202eb6b670b4281199ba90466a008
SjuChefen <madsbraae94@gmail.com>
2024-05-01 10:27:42 +0200

    Commands to image now connected to notify

--- assemblystation/src/main/java/com/bobbyprod/assemblystation/AssemblyStation.java
+++ assemblystation/src/main/java/com/bobbyprod/assemblystation/AssemblyStation.java
@@ -3,16 +3,26 @@ package com.bobbyprod.assemblystation;
 import com.bobbyprod.common.Assets.Asset;
 import com.bobbyprod.common.Assets.AssetType;
 import com.bobbyprod.common.States.AssetState;
+import com.bobbyprod.common.Tasks.ActionType;
 import com.bobbyprod.common.Tasks.Task;
 import com.bobbyprod.common.Interface.IMediator;
+import org.eclipse.paho.client.mqttv3.MqttException;
 
 public class AssemblyStation extends Asset {
 
+    private ASClient mqttClient;
     public AssemblyStation(String id, String name, AssetState state, AssetType type, IMediator mediator) {
         super(id, name, AssetState.IDLE, AssetType.ASSEMBLY_STATION, mediator);
+        this.mqttClient = new ASClient();
+        try {
+            mqttClient.connect();  // Attempt to connect to the MQTT broker
+            mqttClient.subscribe("emulator/status");// Subscribe to the status topic
+        } catch (MqttException e) {
+            System.out.println("Failed to connect to MQTT broker: " + e.getMessage());
+        }
     }
 
-    
+
     public void acceptTask(Task task) {
         System.out.println("AssemblyStation " + getName() + " is processing the task: " + task.getActionType());
         if (processTask(task)) {
@@ -26,16 +36,20 @@ public class AssemblyStation extends Asset {
     @Override
     public boolean processTask(Task task) {
         System.out.println("Assembly " + getName() + " is processing the task: " + task.getActionType());
-        // Placeholder for task processing logic. Real implementation would depend on the task specifics.
-        switch (task.getActionType()) {
-            case ASSEMBLE_ITEM:
-                // Logic to handle assembling an item
-                System.out.println("Assembling item...");
-                return true;
-            default:
-                // Handle other possible actions
-                System.out.println("Unhandled task type: " + task.getActionType());
-                return false;
+        try {
+            switch (task.getActionType()) {
+                case ASSEMBLE_ITEM:
+                    AssemblyCommand startCommand = new AssemblyCommand(1);
+                    mqttClient.publish("emulator/operation", startCommand);
+                    System.out.println("Published assembly start command.");
+                    return true;
+                default:
+                    System.out.println("Unhandled task type: " + task.getActionType());
+                    return false;
+            }
+        } catch (MqttException e) {
+            System.out.println("Failed to publish MQTT message: " + e.getMessage());
+            return false;
         }
     }
 }
