258d8cb75e0dc644cbe6f3c91167a46212649797
SjuChefen <madsbraae94@gmail.com>
2024-05-02 20:41:33 +0200

    Added relevant classes and module

---
+++ drone/src/main/java/com/bobbyprod/drone/Drone.java
@@ -0,0 +1,9 @@
+package com.bobbyprod.drone;
+
+import com.bobbyprod.common.Products.Product;
+
+public class Drone extends Product {
+    public Drone(String name, String id, String location) {
+        super(name, id, location);
+    }
+}
